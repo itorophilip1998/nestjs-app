a35c7c847f15d8ab8f4255ef459518a0f439f9e6
Itorophilip1998 <itorophilip1998@gmail.com>
2025-01-09 12:12:50 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { UsersModule } from './users/users.module';
+import { NinjasModule } from './ninjas/ninjas.module';
 
 @Module({
-  imports: [ProductModule, UsersModule],
+  imports: [ProductModule, UsersModule, NinjasModule],
 })
 export class AppModule {}
