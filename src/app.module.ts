2f0136c2153b279f145c89aacced21e9491edc1c
Itorophilip1998 <itorophilip1998@gmail.com>
2025-01-09 12:11:45 +0100

    update users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductModule, UserModule],
+  imports: [ProductModule, UserModule, UsersModule],
 })
 export class AppModule {}
